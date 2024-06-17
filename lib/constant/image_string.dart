42e168ab0e42ed731478b0f89695e740f9182f1e
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-06-17 22:24:31 +0530

    feat: Add dimensions constants for screen and font sizes

---
+++ lib/constant/image_string.dart
@@ -0,0 +1,6 @@
+class ImageString {
+  static const String splash_animation = 'assets/animation/animation.json';
+  static const String google_logo_image = 'assets/images/google_logo.png';
+  static const String login_screen_image =
+      'assets/images/login_screen.png';
+}
